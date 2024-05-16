4b0dee83a13c30b4883bd242e6d9f5fda53e2e78
Vũ Thành Trung <69748032+vuthanhtrung2010@users.noreply.github.com>
2024-05-16 13:19:56 +0700

    Add files via upload

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+    return (
+      <p id="index-page">
+        Made by trung!
+        <br />
+        Check out{" "}
+        <a href="https://discord.gg/2eTCAqvD8c">the docs at remix.run and join our Discord!</a>.
+      </p>
+    );
+  }
+  
